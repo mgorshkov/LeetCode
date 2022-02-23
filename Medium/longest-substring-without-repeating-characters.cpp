f35bf036bb4e5fad6da2c7e38a055dc9eac65ad0
Mikhail Gorshkov <mikhail.gorshkov@gmail.com>
2022-02-23 15:38:20 +0300

    longest-substring-without-repeating-characters enhanced

--- Medium/longest-substring-without-repeating-characters.cpp
+++ Medium/longest-substring-without-repeating-characters.cpp
@@ -1,32 +1,52 @@
-//
-//  longest-substring-without-repeating-characters.cpp
-//  
-//
-//  Created by Mikhail Gorshkov on 15.11.2021.
-//
-
 //Given a string s, find the length of the longest substring without repeating characters.
-
+//
+// 
+//
+//Example 1:
+//
+//Input: s = "abcabcbb"
+//Output: 3
+//Explanation: The answer is "abc", with the length of 3.
+//Example 2:
+//
+//Input: s = "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+//Example 3:
+//
+//Input: s = "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3.
+//Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+//
+//
+//Constraints:
+//
+//0 <= s.length <= 5 * 104
+//s consists of English letters, digits, symbols and spaces.
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int length = 0;
-        int max_length = 0;
-        set<char> uniq;
-        for (int i = 0; i < s.length(); ++i) {
-            char c = s[i];
-           if (uniq.find(c) == uniq.end()) {
-                length++;
-                if (length > max_length)
-                    max_length = length;
-                uniq.insert(c);
+        if (s.empty())
+            return 0;
+        unordered_set<char> chars;
+        int left = 0;
+        int right = 0;
+        int max_distance = 0;
+        while (right < s.length()) {
+            if (chars.find(s[right]) != chars.end()) {
+                // symbol is found, advance left bound
+                chars.erase(s[left]);
+                ++left;
             } else {
-               uniq.clear();
-               length = 0;
-               while (s[--i] != c)
-                   ;
-           }
+                // symbol is not found, advance right bound
+                chars.insert(s[right]);
+                ++right;
+                int distance = right - left;
+                if (distance > max_distance)
+                    max_distance = distance;
+            }
         }
-        return max_length;
+        return max_distance;
     }
 };
