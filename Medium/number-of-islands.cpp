20c499b14938a866bb402e2f4f53f71c3d7f22a1
Mikhail Gorshkov <mikhail.gorshkov@gmail.com>
2022-05-04 08:50:16 +0300

    +number-of-islands

---
+++ Medium/number-of-islands.cpp
@@ -0,0 +1,69 @@
+//200. Number of Islands
+//Medium
+//
+//13458
+//
+//318
+//
+//Add to List
+//
+//Share
+//Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+//
+//An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+//
+//
+//
+//Example 1:
+//
+//Input: grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//Output: 1
+//Example 2:
+//
+//Input: grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//Output: 3
+//
+//
+//Constraints:
+//
+//m == grid.length
+//n == grid[i].length
+//1 <= m, n <= 300
+//grid[i][j] is '0' or '1'.
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int num_islands = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    ++num_islands;
+                }
+            }
+        }
+        return num_islands;
+    }
+    
+private:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1')
+            return;
+        grid[i][j] = '2';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+};
