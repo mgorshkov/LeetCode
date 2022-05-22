37aae05c877f049e11957f668ac37e9ac39332bf
Mikhail Gorshkov <mikhail.gorshkov@gmail.com>
2022-05-22 20:32:18 +0300

    solution updated

--- Medium/longest-palindromic-substring.cpp
+++ Medium/longest-palindromic-substring.cpp
@@ -1,69 +1,56 @@
+//5. Longest Palindromic Substring
+//Medium
 //
-//  longest-palindromic-substring.cpp
-//  
+//17692
 //
-//  Created by Mikhail Gorshkov on 15.11.2021.
+//1051
 //
-
+//Add to List
+//
+//Share
 //Given a string s, return the longest palindromic substring in s.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "babad"
+//Output: "bab"
+//Explanation: "aba" is also a valid answer.
+//Example 2:
+//
+//Input: s = "cbbd"
+//Output: "bb"
+//
+//
+//Constraints:
+//
+//1 <= s.length <= 1000
+//s consist of only digits and English letters.
 
 class Solution {
 public:
-    string longestCommonSubstring(const string& str1, const string& str2)
-    {
-        if(str1.empty() || str2.empty())
-        {
-            return "";
-        }
- 
-        vector<int> curr(str2.size());
-        vector<int> prev(str2.size());
-        int maxSubstr = 0;
-        string longest;
- 
-        for (int i = 0; i < str1.size(); ++i)
-        {
-            for (unsigned int j = 0; j < str2.size(); ++j)
-            {
-                if (str1[i] != str2[j])
-                {
-                    curr[j] = 0;
-                }
-                else
-                {
-                    if (i == 0 || j == 0)
-                    {
-                        curr[j] = 1;
-                    }
-                    else
-                    {
-                        curr[j] = 1 + prev[j-1];
-                    }
-                    
-                    if (str2.size() - j - 1 != i - curr[j] + 1)
-                        continue;
-                    
-                    if (maxSubstr < curr[j])
-                    {
-                        maxSubstr = curr[j];
-                        longest.clear();
-                    }
-
-                    if (maxSubstr == curr[j])
-                    {
-                        longest += str1.substr(i - maxSubstr + 1, i + 1);
-                    }
-                }
+    string longestPalindrome(const string& s) {
+        int maxLen = 0;
+        string maxPalindrome;
+        for (int i = 0; i < s.size(); ++i) {
+            int len1 = expandPalindrome(i, i, s);
+            int len2 = expandPalindrome(i, i + 1, s);
+            int len{max(len1, len2)};
+            if (len > maxLen) {
+                maxLen = len;
+                maxPalindrome = s.substr(i - (len - 1) / 2, len);
             }
-            std::swap(curr, prev);
         }
-        return longest.substr(0, maxSubstr);
+        return maxPalindrome;
     }
-  
-    string longestPalindrome(string s) {
-        auto s1 = s;
-        std::reverse(s1.begin(), s1.end());
-        
-        return longestCommonSubstring(s, s1);
+    
+private:
+    int expandPalindrome(int left, int right, const string& s) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 };
